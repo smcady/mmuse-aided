5abfe12568f625dfb79cc1ab26f474736f1bf102
Shawn Cady <smcady@gmail.com>
2023-06-09 22:15:46 -0400

    Initial Commit

---
+++ my-app/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['.my-app/src/**/*.{html,ts}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
